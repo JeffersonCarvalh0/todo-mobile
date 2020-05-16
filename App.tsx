2e9008ccb8795f8cf70834661dba23f585e24972
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-05-16 11:56:32 -0300

    feat(Dashboard): add dashboard page

--- App.tsx
+++ App.tsx
@@ -1,19 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components/native';
-import {SafeAreaView, StatusBar, View, Text} from 'react-native';
-import {withTheme, Button} from 'react-native-paper';
+import {SafeAreaView, StatusBar, View} from 'react-native';
+import {withTheme} from 'react-native-paper';
 import {Switch, Route, Redirect, BackButton} from 'react-router-native';
 import AsyncStorage from '@react-native-community/async-storage';
-import {Link} from 'react-router-native';
 
 import Login from './src/pages/Login';
 import SignUp from './src/pages/SignUp';
+import Dashboard from './src/pages/Dashboard';
 
 const Container = withTheme(
   styled(View)`
     flex: 1;
     justify-content: center;
     padding-horizontal: 20px;
+    padding-vertical: 20px;
     background-color: ${(props) => props.theme.colors.background};
   `,
 );
@@ -44,18 +45,7 @@ const App = () => {
           <Switch>
             <Route path="/login" component={Login} />
             <Route path="/signup" component={SignUp} />
-            <Route path="/dashboard">
-              <Text>Dashboard</Text>
-              <Link to="/login" replace>
-                <Button
-                  mode="contained"
-                  onPress={async () => {
-                    await AsyncStorage.setItem('token', '');
-                  }}>
-                  Logoff
-                </Button>
-              </Link>
-            </Route>
+            <Route path="/dashboard" component={Dashboard} />
           </Switch>
         </Container>
       </StyledSafeArea>
